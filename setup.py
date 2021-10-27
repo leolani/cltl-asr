9b5be350f44d4257e06e41714fc704f4a470f235
Thomas Baier <tbaier@math.bme.hu>
2021-10-27 12:00:09 +0200

    Release 0.0.dev3 for PyPI

--- setup.py
+++ setup.py
@@ -20,16 +20,16 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.7',
-    install_requires=['numpy'],
+    install_requires=['numpy>=1.21.2'],
     extras_require={
         "impl": [
-            "cffi",
-            "importlib_resources",
-            "jiwer",
-            "sounddevice",
-            "soundfile",
-            "torch",
-            "transformers"
+            "cffi>=1.14.6",
+            "importlib_resources>=5.2.2",
+            "jiwer>=2.2.0",
+            "sounddevice>=0.4.2",
+            "soundfile>=0.10.3.post1",
+            "torch==1.9.0",
+            "transformers==4.10.0"
         ],
         "service": [
             "cltl.backend",
